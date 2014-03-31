7fb2493a60e49e638b3b401efe71a2a8bb81c208
sx349 <xuyj.12@sem.tsinghua.edu.cn>
2014-03-31 19:53:04 +0800

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -74,8 +74,9 @@ GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     var value = Math.random() < 0.9 ? 2 : 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
-
     this.grid.insertTile(tile);
+    if (!this.movesAvailable()) {tile.value = tile.value / 2; this.grid.insertTile(tile);}
+    while (!this.movesAvailable()) {tile.value = tile.value * 2; this.grid.insertTile(tile);}
   }
 };
 
